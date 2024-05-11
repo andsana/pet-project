8e3c04ede5f1de1b7f485ace5e813bab4701770c
Susanna Andreeva <sanadex90@gmail.com>
2024-05-12 04:33:41 +0900

    Add store.ts, hooks.ts, axiosApi.ts, links.ts

---
+++ frontend/src/store/store.ts
@@ -0,0 +1,17 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
+
+const rootReducer = combineReducers({});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, PAUSE, PERSIST, REHYDRATE, PURGE, REGISTER],
+      },
+    }),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
